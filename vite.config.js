60d554b01f8520d8db6236b5eeea9eb91d4bdd07
rosham07 <roshambajgain7@gmail.com>
2025-05-06 16:39:18 +0545

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss(),
-  ],
+  base: '/Global-ladder/', // 🔁 Replace this with your GitHub repo name
+  plugins: [react()],
 })
